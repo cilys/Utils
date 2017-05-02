306e73d5ac24b1a5616fc88109a764680568f7ff
cily-code <1321851830@qq.com>
2017-05-02 16:14:34 +0800

    barcode

---
+++ utils_barcode/src/main/java/com/cily/utils/barcode/decoding/Conf.java
@@ -0,0 +1,18 @@
+package com.cily.utils.barcode.decoding;
+
+/**
+ * user:cily
+ * time:2017/5/2
+ * desc:
+ */
+
+public interface Conf {
+    int AUTO_FOCUS = 10001;
+    int REATART_PREVIEW = 10002;
+    int DECODE_SUCCESS = 10003;
+    int DECODE_FAILED = 10004;
+    int RETURN_SCAN_RESULT = 10005;
+    int LAUNCH_PRODUCT_QUERY = 10006;
+    int DECODE = 10007;
+    int QUIT = 10008;
+}
